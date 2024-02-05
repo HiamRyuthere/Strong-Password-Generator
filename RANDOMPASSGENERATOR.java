ac4c9c98b93bb3f7f2bf9ed2489a873ac26feb48
HiamRyuthere <159010140+HiamRyuthere@users.noreply.github.com>
2024-02-05 15:20:44 +0530

    THIS IS THE CODE
    THIS IS THE CODE

---
+++ RANDOMPASSGENERATOR.java
@@ -0,0 +1,32 @@
+
+// THIS IS JAVA CODE TO GENERATE RANDOM PASSWORD 
+
+import java.util.Random;
+
+public class RANDOMPASSGENERATOR{
+    // private static Random random = new Random();// IDK WHY TO WRITE IT HERE
+    static String PasswordGen() {// IDK WHY STATIC HERE
+        Random random = new Random();
+
+        char[] charArray = new char[15];
+        for (int i = 0; i < 15; i++) {
+            int rand = random.nextInt(58) + 65;
+            charArray[i] = (char) rand;
+            // continue;
+
+        }
+
+        // return CharArray;
+        String str = new String(charArray);
+        return str;
+    }
+
+    public static void main(String[] args) {
+
+        String password = PasswordGen();
+        System.out.println(password);
+        // THIS FUNCTION WILL CREATE 15 DIGIT RANDOM PASSWORD
+
+    }
+
+}
